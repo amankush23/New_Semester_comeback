1939d5442f1d2a43be3050247b0aa7f131662240
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-08-02 18:44:37 +0000

    744 leetcode

---
+++ Find_Smallest_Letter_Greater_Than_Target.java
@@ -0,0 +1,20 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int length = letters.length;
+        if(letters[length-1] <= target || target < letters[0] ){
+            return letters[0];
+        }
+        int l = 0, r = length-1;
+        while(l+1 < r){
+            int mid = l+(r-l)/2;
+            if(letters[mid] <= target){
+                l=mid;
+            }
+            else{
+                r=mid;
+            }
+
+        }
+            return letters[r];
+    }
+}
