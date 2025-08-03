74dd694061b4251fefae53752a9066a9010ccae9
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-08-03 23:08:04 +0530

    Create 183-CustomersWhoNeverOrder

---
+++ 183-CustomersWhoNeverOrder/version/java/183-CustomersWhoNeverOrder_20250803_230802.java
@@ -0,0 +1,29 @@
+// Last updated: 8/3/2025, 11:08:02 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right  = invertTree(root.right);
+
+        root.right = left;
+        root.left = right;
+        return root;
+    }
+}
