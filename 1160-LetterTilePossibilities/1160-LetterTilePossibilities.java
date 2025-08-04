c05f8e61326d6ca1c490970ef0ac2db53c281ae8
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-08-04 23:10:47 +0530

    Create 1160-LetterTilePossibilities

---
+++ 1160-LetterTilePossibilities/1160-LetterTilePossibilities.java
@@ -0,0 +1,22 @@
+// Last updated: 8/4/2025, 11:10:45 PM
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        int[] freq = new int[26];
+        for (char c : tiles.toCharArray()) {
+            freq[c - 'A']++;
+        }
+        return dfs(freq);
+    }
+
+    private int dfs(int[] freq) {
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0) continue;
+            count++;
+            freq[i]--;
+            count += dfs(freq);
+            freq[i]++;
+        }
+        return count;
+    }
+}
