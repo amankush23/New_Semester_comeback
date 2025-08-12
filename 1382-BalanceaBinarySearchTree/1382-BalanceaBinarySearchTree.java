bb36199e1aedf9c27375e6c65e1303868eecfe4f
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-08-12 21:57:38 +0530

    Create 1382-BalanceaBinarySearchTree

---
+++ 1382-BalanceaBinarySearchTree/1382-BalanceaBinarySearchTree.java
@@ -0,0 +1,41 @@
+// Last updated: 8/12/2025, 9:57:37 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<TreeNode> sortedArr= new ArrayList<>();
+    public TreeNode balanceBST(TreeNode root) {
+        inorder(root);
+        return Create_Tree(0, sortedArr.size()-1);
+        
+    }
+    void inorder(TreeNode root){
+        if(root == null) return;
+        inorder(root.left);
+        sortedArr.add(root);
+        inorder(root.right);
+    }
+    TreeNode Create_Tree(int si, int ei){
+        if(si>ei){
+            return null;
+        }
+        int mid = (si+ei)/2;
+        TreeNode nn = sortedArr.get(mid);
+        nn.left=Create_Tree(si, mid-1);
+        nn.right= Create_Tree(mid+1, ei);
+        return nn;
+
+    }
+}
