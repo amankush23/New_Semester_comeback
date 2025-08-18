ee31d9dfb59dd84977c48051cf8517ea441fff50
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-08-18 22:06:17 +0530

    Create 1008-ConstructBinarySearchTreefromPreorderTraversal

---
+++ 1008-ConstructBinarySearchTreefromPreorderTraversal/1008-ConstructBinarySearchTreefromPreorderTraversal.java
@@ -0,0 +1,48 @@
+// Last updated: 8/18/2025, 10:06:15 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int idx = 0; // pointer to track preorder array
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return build(preorder, Integer.MAX_VALUE);
+    }
+
+    private TreeNode build(int[] preorder, int bound) {
+        if (idx == preorder.length || preorder[idx] > bound) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[idx++]);
+        root.left = build(preorder, root.val);
+        root.right = build(preorder, bound);
+        return root;
+    }
+}
