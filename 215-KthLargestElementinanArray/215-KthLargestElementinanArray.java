11c9889c606baf7f6e4b460a29e630ce516f389f
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-08-20 12:18:43 +0530

    Create 215-KthLargestElementinanArray

---
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.java
@@ -0,0 +1,19 @@
+// Last updated: 8/20/2025, 12:18:43 PM
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        return Kthlargerstelement(nums, k);
+    }
+    public static int Kthlargerstelement(int[] arr, int k){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0 ; i < k; i++){
+            pq.add(arr[i]);
+        }
+        for(int i = k ; i< arr.length; i++){
+            if(arr[i]>pq.peek()){
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+        return pq.peek();
+    }
+}
