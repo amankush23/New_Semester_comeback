6b803e9e52f75200404c31a371e1766b2f3934db
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-08-28 22:46:02 +0530

    Create recover_BST.java

---
+++ recover_BST.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode first = null, second = null, prev = new TreeNode(Integer.MIN_VALUE);
+
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+
+        // Swap the values of the two incorrect nodes
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void inorder(TreeNode root) {
+        if (root == null) return;
+
+        inorder(root.left);
+
+        // If previous node value > current node value → violation
+        if (first == null && prev.val > root.val) {
+            first = prev;
+        }
+        if (first != null && prev.val > root.val) {
+            second = root;
+        }
+
+        prev = root; // move prev forward
+        inorder(root.right);
+    }
+}
