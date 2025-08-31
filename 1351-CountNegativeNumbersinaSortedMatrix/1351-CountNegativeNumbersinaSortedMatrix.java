2a26d0621f279b3bbfe976c2948babd91fbef620
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-08-31 14:15:47 +0530

    File updated at 8/31/2025, 2:15:47 PM

--- 1351-CountNegativeNumbersinaSortedMatrix/1351-CountNegativeNumbersinaSortedMatrix.java
+++ 1351-CountNegativeNumbersinaSortedMatrix/1351-CountNegativeNumbersinaSortedMatrix.java
@@ -1,17 +1,19 @@
-// Last updated: 8/31/2025, 2:13:08 PM
+// Last updated: 8/31/2025, 2:15:47 PM
 class Solution {
-    public int countNegatives(int[][] grid) {
-        return count(grid);
-    }
-    public int count(int[][] matrix ){
-        int c=0;
-        for(int i = 0 ; i < matrix.length; i++){
-            for(int j = 0; j < matrix[i].length; j++){
-                if(matrix[i][j] < 0){
-                    c++;
-                }
+     public int countNegatives(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int row = 0; 
+        int col = grid[0].length-1;
+        int count = 0;
+        while(row < rows && col >= 0){
+            if(grid[row][col] >= 0){
+                row++;
+            } else {
+                count += rows-row;
+                col--;
             }
         }
-        return c;
+        return count;
     }
 }
