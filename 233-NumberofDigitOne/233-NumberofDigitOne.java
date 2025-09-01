5b5c6b87b2f2615ebecd70e8fd54d10463019d9b
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-09-01 23:12:54 +0530

    File updated at 9/1/2025, 11:12:53 PM

--- 233-NumberofDigitOne/233-NumberofDigitOne.java
+++ 233-NumberofDigitOne/233-NumberofDigitOne.java
@@ -1,8 +1,25 @@
-// Last updated: 9/1/2025, 11:12:10 PM
+// Last updated: 9/1/2025, 11:12:53 PM
 class Solution {
     public int countDigitOne(int n) {
-        long ans=0, factor=1; 
-        while(factor<=n){ long high=n/(factor*10), curr=(n/factor)%10, low=n%factor; ans += (curr==0? high*factor : (curr==1? high*factor+low+1 : (high+1)*factor)); factor*=10; }
-        return (int)ans;
+        long factor = 1; // current digit position
+        long count = 0;
+        
+        while (factor <= n) {
+            long high = n / (factor * 10);
+            long curr = (n / factor) % 10;
+            long low = n % factor;
+            
+            if (curr == 0) {
+                count += high * factor;
+            } else if (curr == 1) {
+                count += high * factor + (low + 1);
+            } else {
+                count += (high + 1) * factor;
+            }
+            
+            factor *= 10;
+        }
+        
+        return (int) count;
     }
 }
