aa111192923e1db4d277059e089472929c42db08
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-09-01 23:58:55 +0530

    Create 2864-MaximumOddBinaryNumber

---
+++ 2864-MaximumOddBinaryNumber/2864-MaximumOddBinaryNumber.java
@@ -0,0 +1,25 @@
+// Last updated: 9/1/2025, 11:58:54 PM
+class Solution {
+    public String maximumOddBinaryNumber(String binaryString) {
+        int countOnes = 0;
+        StringBuilder resultBuilder = new StringBuilder();
+        
+        for (char ch : binaryString.toCharArray()) {
+            if (ch == '1') {
+                countOnes++;
+            }
+        }
+        
+        for (int i = 0; i < binaryString.length() - 1; i++) {
+            if (countOnes > 1) {
+                countOnes--;
+                resultBuilder.append('1');
+            } else {
+                resultBuilder.append('0');
+            }
+        }
+        
+        resultBuilder.append('1');
+        return resultBuilder.toString();
+    }
+}
