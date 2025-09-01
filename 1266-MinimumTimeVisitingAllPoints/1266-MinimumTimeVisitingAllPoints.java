c5f47dc691b0ead96b307d5543552bd27e162034
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-09-01 23:58:05 +0530

    Create 1266-MinimumTimeVisitingAllPoints

---
+++ 1266-MinimumTimeVisitingAllPoints/1266-MinimumTimeVisitingAllPoints.java
@@ -0,0 +1,20 @@
+// Last updated: 9/1/2025, 11:58:04 PM
+class Solution {
+    
+    public int toTime(int[] from, int[] to) {
+        int xDiff = Math.abs(from[0] - to[0]);
+        int yDiff = Math.abs(from[1] - to[1]);
+        
+        return Math.max(xDiff, yDiff);
+    }
+    
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int time = 0;
+    
+        for (int i = 1; i < points.length; i++) {
+            time += toTime(points[i - 1], points[i]);
+        }
+        
+        return time;
+    }
+}
