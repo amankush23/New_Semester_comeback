1807633df9066f6969b6add2e2481b84ff45836c
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-09-03 00:25:09 +0530

    Create 392-IsSubsequence

---
+++ 392-IsSubsequence/392-IsSubsequence.java
@@ -0,0 +1,16 @@
+// Last updated: 9/3/2025, 12:25:09 AM
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        char[] arr = s.toCharArray();
+        char[] array = t.toCharArray();
+        int i=0;
+
+        for(int j=0;j<array.length && i<arr.length;j++){
+            if(arr[i]==array[j]){
+                i++;
+            }
+        }
+
+        return i == arr.length;
+    }
+}
