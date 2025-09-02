bc8ba82b85c1d29edd2d5cd4244e0c07dc473b3e
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-09-02 23:22:44 +0530

    File updated at 9/2/2025, 11:22:43 PM

--- 682-BaseballGame/682-BaseballGame.java
+++ 682-BaseballGame/682-BaseballGame.java
@@ -1,25 +1,23 @@
-// Last updated: 9/2/2025, 11:21:47 PM
+// Last updated: 9/2/2025, 11:22:43 PM
 class Solution {
-    int index=-1,total=0;
+    int index=-1;
     List<Integer>list=new ArrayList();
     public int calPoints(String[] operations) {
         for(String str:operations){
             index = list.size()-1;
-            boolean flag=true;
             if(str.matches("-?\\d+"))
                 list.add(Integer.parseInt(str));
             else if(str.equals("+"))
                 list.add(list.get(index)+list.get(index-1));
             else if(str.equals("D"))
                 list.add(list.get(index)*2);
-            else{
-                flag=false;
-                total-=list.get(index);
+            else
                 list.remove(index);
-                }
-            if(flag)
-                total+=list.get(index+1);
+         
         }
+        int total=0;
+            for(int i:list)
+                total+=i;
         return total;
     }
 }
