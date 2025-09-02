f463266e15793cee73b4cfabfbe299d601ef572e
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-09-02 23:23:44 +0530

    File updated at 9/2/2025, 11:23:43 PM

--- 682-BaseballGame/682-BaseballGame.java
+++ 682-BaseballGame/682-BaseballGame.java
@@ -1,23 +1,30 @@
-// Last updated: 9/2/2025, 11:22:43 PM
+// Last updated: 9/2/2025, 11:23:43 PM
 class Solution {
-    int index=-1;
-    List<Integer>list=new ArrayList();
     public int calPoints(String[] operations) {
-        for(String str:operations){
-            index = list.size()-1;
-            if(str.matches("-?\\d+"))
-                list.add(Integer.parseInt(str));
-            else if(str.equals("+"))
-                list.add(list.get(index)+list.get(index-1));
-            else if(str.equals("D"))
-                list.add(list.get(index)*2);
-            else
-                list.remove(index);
-         
+        int[] records = new int[operations.length];
+        int pointer = 0;
+        
+        int sum = 0;
+
+        for (String s : operations) {
+
+            if (s.equals("+")) {
+                records[pointer] = records[pointer - 1] + records[pointer - 2];
+                pointer++;
+            } else if (s.equals("D")) {
+                records[pointer] = records[pointer - 1] * 2;
+                pointer++;
+            } else if (s.equals("C")) {
+                records[--pointer] = 0;
+            } else {
+                records[pointer++] = Integer.parseInt(s);
+            }
         }
-        int total=0;
-            for(int i:list)
-                total+=i;
-        return total;
+
+        for (int rec : records) {
+            sum += rec;
+        }
+
+        return sum;
     }
 }
