e3e1ea3001f1e84cf1da870b7091edc218443628
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-09-02 23:18:16 +0530

    Create 1021-RemoveOutermostParentheses

---
+++ 1021-RemoveOutermostParentheses/1021-RemoveOutermostParentheses.java
@@ -0,0 +1,23 @@
+// Last updated: 9/2/2025, 11:18:16 PM
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int cnt = 0;
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                cnt--;
+            }
+
+            if (cnt != 0) {
+                ans.append(s.charAt(i));
+            }
+
+            if (s.charAt(i) == '(') {
+                cnt++;
+            }
+        }
+
+        return ans.toString();
+    }
+}
