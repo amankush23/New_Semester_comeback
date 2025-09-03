c0a6fe4c117e1b60c34b5aac8f548f97980d0bc4
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-09-04 00:06:31 +0530

    Create 1035-UncrossedLines

---
+++ 1035-UncrossedLines/1035-UncrossedLines.java
@@ -0,0 +1,23 @@
+// Last updated: 9/4/2025, 12:06:30 AM
+class Solution {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        return lcsbu(nums1,nums2);
+    }
+    public static int lcsbu(int[] text1, int[] text2) {
+		int[][] dp = new int[text1.length + 1][text2.length + 1];
+		for (int i = 1; i < dp.length; i++) {
+			for (int j = 1; j < dp[0].length; j++) {
+				int ans = 0;
+				if (text1[i - 1] == text2[j - 1]) {
+					ans = 1 + dp[i - 1][j - 1];
+				} else {
+					int f = dp[i - 1][j];
+					int s = dp[i][j - 1];
+					ans = Math.max(f, s);
+				}
+				dp[i][j] = ans;
+			}
+		}
+		return dp[dp.length - 1][dp[0].length - 1];
+	}
+}
