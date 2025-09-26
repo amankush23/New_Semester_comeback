52cafa78c529c8bce78be9513badc47694b68083
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-09-26 14:31:26 +0530

    Create 875-KokoEatingBananas

---
+++ 875-KokoEatingBananas/875-KokoEatingBananas.java
@@ -0,0 +1,34 @@
+// Last updated: 9/26/2025, 2:31:26 PM
+class Solution {
+
+    public boolean canEat(int [] piles,int h,int mid) {
+        int ch = 0; // total hours
+        for(int i = 0; i < piles.length;i++) {
+            int pile = piles[i];
+            int hour = pile / mid;
+            if(pile % mid > 0) {
+                hour += 1;
+            }
+            ch += hour;
+            if(ch > h) {
+               return false;
+            }
+        }
+        return true;
+    }
+    public int minEatingSpeed(int[] piles, int ho) {
+        int l =1;
+        int h = Arrays.stream(piles).max().getAsInt();
+        int eat = 0;
+        while(l <= h) {
+            int mid = l + (h - l)/2;
+            if(canEat(piles, ho, mid)) {
+                eat = mid;
+                h = mid-1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return eat;
+    }
+}
