dd8125c8aba244e90ca1b1ad4545d5263a377516
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-09-27 09:12:13 +0530

    File updated at 9/27/2025, 9:12:11 AM

--- 3541-FindMostFrequentVowelandConsonant/3541-FindMostFrequentVowelandConsonant.java
+++ 3541-FindMostFrequentVowelandConsonant/3541-FindMostFrequentVowelandConsonant.java
@@ -1,26 +1,27 @@
-// Last updated: 8/20/2025, 10:59:32 PM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// Last updated: 9/27/2025, 9:12:11 AM
+import java.util.*;
+
 class Solution {
-    public boolean checkTree(TreeNode root) {
-        if(root.left.val+root.right.val == root.val){
-            return true;
+    public int maxFreqSum(String s) {
+        HashMap<Character, Integer> m = new HashMap<>();
+        
+        for (char c : s.toCharArray()) {
+            m.put(c, m.getOrDefault(c, 0) + 1);
         }
-        else{
-            return false;
+
+        int vMax = 0;
+        int cMax = 0;
+        HashSet<Character> v = new HashSet<>(Arrays.asList('a','e','i','o','u'));
+
+        for (char k : m.keySet()) {
+            int f = m.get(k);
+            if (v.contains(k)) {
+                if (f > vMax) vMax = f;
+            } else {
+                if (f > cMax) cMax = f;
+            }
         }
+
+        return vMax + cMax;
     }
 }
