96f6c5a8bdb24f0d7549c79648b0d9128e34cc56
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-10-05 22:59:05 +0530

    File updated at 10/5/2025, 10:59:04 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
@@ -1,4 +1,4 @@
-// Last updated: 10/5/2025, 10:57:43 PM
+// Last updated: 10/5/2025, 10:59:04 PM
 class Solution {
     public int removeDuplicates(int[] nums) {
         if (nums.length == 0) return 0;
@@ -6,8 +6,7 @@ class Solution {
         int k = 1; // index of the next unique element
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[i - 1]) {
-                nums[k] = nums[i];
-                k++;
+                nums[k++] = nums[i];
             }
         }
         return k;
