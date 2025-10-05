3d4f210c61f6ac41a1fdc9dc351822b9b640fd29
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-10-05 11:44:27 +0530

    Create 526-BeautifulArrangement

---
+++ 526-BeautifulArrangement/526-BeautifulArrangement.java
@@ -0,0 +1,23 @@
+// Last updated: 10/5/2025, 11:44:27 AM
+class Solution {
+    int res = 0;
+    public int countArrangement(int n) {
+        if (n == 0) return 0;
+        backtrack(1, n, new int[n+1]);
+        return res;
+    }
+
+    private void backtrack(int pos, int n, int[] usedValueArr) {
+        if (pos > n) {
+            res++;
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (usedValueArr[i] == 0 && (pos % i == 0 || i % pos == 0)) {
+                usedValueArr[i] = 1;
+                backtrack(pos+1, n, usedValueArr);
+                usedValueArr[i] = 0;
+            }
+        }
+    }
+}
