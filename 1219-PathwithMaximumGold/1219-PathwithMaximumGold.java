e450075c2427c2766d36fadeecc786bbbed9089b
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-10-06 10:42:44 +0530

    File updated at 10/6/2025, 10:42:44 AM

--- 1219-PathwithMaximumGold/1219-PathwithMaximumGold.java
+++ 1219-PathwithMaximumGold/1219-PathwithMaximumGold.java
@@ -1,4 +1,4 @@
-// Last updated: 10/5/2025, 10:34:07 PM
+// Last updated: 10/6/2025, 10:42:44 AM
 class Solution {
     public int getMaximumGold(int[][] grid) {
         int m = grid.length;
@@ -17,7 +17,7 @@ class Solution {
         return maxGold;
     }
 
-    int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    // int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     public int DFS(int[][] grid, int i, int j) {
         int m = grid.length;
         int n = grid[0].length;
@@ -29,13 +29,18 @@ class Solution {
         grid[i][j] = 0;
 
         int maxGold = 0;
-        for(int[] dir : directions) {
-            int new_i = i + dir[0];
-            int new_j = j + dir[1];
+        // for(int[] dir : directions) {
+        //     int new_i = i + dir[0];
+        //     int new_j = j + dir[1];
 
-            maxGold = Math.max(maxGold, DFS(grid, new_i, new_j));
-        }
+        //     maxGold = Math.max(maxGold, DFS(grid, new_i, new_j));
+        // }
 
+
+        maxGold = Math.max(maxGold, DFS(grid, i + 1, j));
+        maxGold = Math.max(maxGold, DFS(grid, i - 1, j));
+        maxGold = Math.max(maxGold, DFS(grid, i, j + 1));
+        maxGold = Math.max(maxGold, DFS(grid, i, j - 1));
         grid[i][j] = originalGoldValue;
         return originalGoldValue + maxGold;
     }
