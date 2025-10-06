b0ea25ab53bb975e67374dc99f8be1f9700126c1
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-10-06 14:49:38 +0530

    File updated at 10/6/2025, 2:49:38 PM

--- 44-WildcardMatching/44-WildcardMatching.java
+++ 44-WildcardMatching/44-WildcardMatching.java
@@ -1,26 +1,39 @@
-// Last updated: 10/6/2025, 2:09:17 PM
+// Last updated: 10/6/2025, 2:49:38 PM
+import java.util.*;
+
 class Solution {
     public boolean isMatch(String s, String p) {
-        int m = s.length();
-        int n = p.length();
-        boolean[][] dp  = new boolean[m+1][n+1];
-        dp[0][0] = true;
-            for(int j =1 ; j <= n;j++){
-                if(p.charAt(j-1) == '*'){
-                    dp[0][j] = dp[0][j-1];
-                }
+        int[][] dp = new int[s.length() + 1][p.length() + 1];
+        for (int[] row : dp) Arrays.fill(row, -1);
+        return match(s, p, 0, 0, dp);
+    }
+
+    public boolean match(String s, String p, int i, int j, int[][] dp) {
+        if (i >= s.length() && j >= p.length()) {
+            return true;
+        }
+        if (j >= p.length()) {
+            return false;
+        }
+        if (i >= s.length()) {
+            while (j < p.length()) {
+                if (p.charAt(j) != '*') return false;
+                j++;
             }
-            for(int i = 1; i <= m; i++){
-                for(int j = 1; j <=n; j++){
-                    char pc = p.charAt(j-1);
-                    if(pc == '*'){
-                        dp[i][j] = dp[i][j-1] || dp[i-1][j];
-                    }
-                    else if(pc == '?' || s.charAt(i-1) == pc){
-                        dp[i][j] = dp[i-1][j-1];
-                    }
-                }
-            }
-        return dp[m][n];
+            return true;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j] == 1;
+        }
+        boolean ans;
+        if (p.charAt(j) == '?' || s.charAt(i) == p.charAt(j)) {
+            ans = match(s, p, i + 1, j + 1, dp);
+        } else if (p.charAt(j) == '*') {
+            ans = match(s, p, i + 1, j, dp) || match(s, p, i, j + 1, dp);
+        } else {
+            ans = false;
+        }
+        dp[i][j] = ans ? 1 : 0;
+        return ans;
     }
 }
