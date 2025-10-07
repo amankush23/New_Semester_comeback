bdcbf920fd470217d2f8717d036266df0d710e85
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-10-07 22:03:52 +0530

    Create

---
+++ problem-1759854830648/problem-1759854830648.java
@@ -0,0 +1,29 @@
+// Last updated: 10/7/2025, 10:03:50 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+}
