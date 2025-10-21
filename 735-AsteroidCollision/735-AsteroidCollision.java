a135a8b5fc987c54abf209cf3ac9bd30daba6cf4
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-10-21 22:04:19 +0530

    Create 735-AsteroidCollision

---
+++ 735-AsteroidCollision/735-AsteroidCollision.java
@@ -0,0 +1,35 @@
+// Last updated: 10/21/2025, 10:04:20 PM
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        return AC(asteroids);
+    }
+    public int[] AC(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int a : asteroids) {
+            while (!stack.isEmpty() && a < 0 && stack.peek() > 0) {
+                int sum = a + stack.peek();
+
+                if (sum < 0) {
+                    stack.pop();
+
+                } else if (sum > 0) {
+                    a = 0;
+
+                } else {
+                    stack.pop();
+                    a = 0;
+                }
+
+            }
+            if (a != 0)
+                stack.push(a);
+
+        }
+        int arr[] = new int[stack.size()];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            arr[i] = stack.pop();
+        }
+        return arr;
+    }
+}
