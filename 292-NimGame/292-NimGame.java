7e13ef0de75d0c06c90c682094efe7de325c0ac3
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-11-07 21:56:34 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,10 @@
+// Last updated: 11/7/2025, 9:56:33 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        
+        if (n % 4 != 0)
+            return true;
+        else
+            return false;
+    }
+}
