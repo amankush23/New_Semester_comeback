c685db4387581ef377229ca56b90b2a309ae613a
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-12-19 22:45:09 +0530

    File updated at 12/19/2025, 10:45:08 PM

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,25 +1,25 @@
-// Last updated: 8/4/2025, 11:13:38 PM
-class Solution {
-    public int trap(int[] height) {
-        return trapping_rain_water(height);
-
-    }
-    public static int trapping_rain_water(int arr[]){
-        int n=arr.length;
-        int left[]= new int[n];
-        int right[]=new int[n];
-        left[0]= arr[0];
-        right[n-1]=arr[n-1];
-        //left array
-        for(int i=1;i<n;i++){
-            left[i]=Math.max(left[i-1],arr[i]);
-        }
-        for(int i=n-2;i>=0;i--){
-            right[i]=Math.max(right[i+1],arr[i]);
-        }
-        int sum=0;
-        for(int i=0;i<arr.length;i++){
-            sum=sum+Math.min(left[i],right[i])-arr[i];
-        }return sum;
-    }
-}
+// Last updated: 12/19/2025, 10:45:08 PM
+1class Solution {
+2    public int trap(int[] height) {
+3        return trapping_rain_water(height);
+4
+5    }
+6    public static int trapping_rain_water(int arr[]){
+7        int n=arr.length;
+8        int left[]= new int[n];
+9        int right[]=new int[n];
+10        left[0]= arr[0];
+11        right[n-1]=arr[n-1];
+12        //left array
+13        for(int i=1;i<n;i++){
+14            left[i]=Math.max(left[i-1],arr[i]);
+15        }
+16        for(int i=n-2;i>=0;i--){
+17            right[i]=Math.max(right[i+1],arr[i]);
+18        }
+19        int sum=0;
+20        for(int i=0;i<arr.length;i++){
+21            sum=sum+Math.min(left[i],right[i])-arr[i];
+22        }return sum;
+23    }
+24}
