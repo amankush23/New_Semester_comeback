81347019cd87da163d03758546ae9ef21870d381
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-03 12:20:54 +0530

    File updated at 1/3/2026, 12:20:53 PM

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,15 +1,16 @@
-// Last updated: 8/4/2025, 11:13:17 PM
-class Solution {
-    public int climbStairs(int n) {
-        if (n <= 2) return n; // Base cases: f(1) = 1, f(2) = 2
-
-        int first = 1, second = 2;
-        for (int i = 3; i <= n; i++) {
-            int third = first + second;
-            first = second;
-            second = third;
-        }
-
-        return second;
-    }
-}
+// Last updated: 1/3/2026, 12:20:53 PM
+1class Solution {
+2    public int climbStairs(int n) {
+3        if (n <= 2) return n; // Base cases: f(1) = 1, f(2) = 2
+4
+5        int first = 1, second = 2;
+6        for (int i = 3; i <= n; i++) {
+7            int third = first + second;
+8            first = second;
+9            second = third;
+10        }
+11
+12        return second;
+13    }
+14}
+15
