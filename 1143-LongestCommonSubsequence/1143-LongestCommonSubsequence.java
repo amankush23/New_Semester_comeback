fae5f371845bd5373ed8848707c67d3276467d19
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-04 11:12:49 +0530

    File updated at 1/4/2026, 11:12:48 AM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,28 +1,28 @@
-// Last updated: 9/3/2025, 11:24:33 PM
-class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length()+1][text2.length()+1];
-        for(int[] a:dp){
-            Arrays.fill(a, -1);
-        }
-        return LCS(text1, text2, 0,0, dp);
-    }
-    public int LCS(String text1, String text2, int i, int j,int[][] dp){
-        if(i == text1.length() || j == text2.length()){
-            return 0;
-        }
-        if(dp[i][j] != -1 ){
-            return dp[i][j];
-        }
-        int ans = 0;
-        if(text1.charAt(i) == text2.charAt(j)){
-            ans = 1+LCS(text1, text2, i+1, j+1, dp);
-        }
-        else{
-            int f = LCS(text1, text2, i+1, j,dp);
-            int s = LCS(text1, text2, i, j+1,dp);
-            ans=Math.max(f, s);
-        }
-        return dp[i][j] = ans;
-    }
-}
+// Last updated: 1/4/2026, 11:12:48 AM
+1class Solution {
+2    public int longestCommonSubsequence(String text1, String text2) {
+3        int[][] dp = new int[text1.length()][text2.length()];
+4        for(int[] arr : dp) Arrays.fill(arr, -1);
+5        return LCS(text1, text2, 0,0,dp);
+6    }
+7    public int LCS(String text1, String text2, int i, int j,int[][] dp){
+8        if(i == text1.length() || j == text2.length()){
+9            return 0;
+10        }
+11        if(dp[i][j] != -1){
+12            return dp[i][j];
+13        }
+14        
+15        int ans = 0;
+16        if(text1.charAt(i) == text2.charAt(j)){
+17            ans = 1+LCS(text1, text2, i+1, j+1,dp);
+18        }
+19        else{
+20            int f = LCS(text1, text2, i+1, j,dp);
+21            int s = LCS(text1, text2, i, j+1,dp);
+22            ans=Math.max(f, s);
+23        }
+24        return dp[i][j]=ans;
+25        
+26    }
+27}
