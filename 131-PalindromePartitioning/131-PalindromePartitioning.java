dbc515b41c39b2122cc56b51c8f665429c6c8d8b
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-06 12:49:07 +0530

    File updated at 1/6/2026, 12:49:07 PM

--- 131-PalindromePartitioning/131-PalindromePartitioning.java
+++ 131-PalindromePartitioning/131-PalindromePartitioning.java
@@ -1,31 +1,31 @@
-// Last updated: 9/22/2025, 11:43:33 PM
-public class Solution {
-    public List<List<String>> partition(String s) {
-        List<List<String>> result = new ArrayList<>();
-        backtrack(s, 0, new ArrayList<>(), result);
-        return result;
-    }
-
-    private void backtrack(String s, int start, List<String> path, List<List<String>> result) {
-        if (start == s.length()) {
-            result.add(new ArrayList<>(path));
-            return;
-        }
-        for (int end = start + 1; end <= s.length(); end++) {
-            if (isPalindrome(s, start, end - 1)) {
-                path.add(s.substring(start, end));
-                backtrack(s, end, path, result);
-                path.remove(path.size() - 1);
-            }
-        }
-    }
-
-    private boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left++) != s.charAt(right--)) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
+// Last updated: 1/6/2026, 12:49:07 PM
+1public class Solution {
+2    public List<List<String>> partition(String s) {
+3        List<List<String>> result = new ArrayList<>();
+4        backtrack(s, 0, new ArrayList<>(), result);
+5        return result;
+6    }
+7
+8    private void backtrack(String s, int start, List<String> path, List<List<String>> result) {
+9        if (start == s.length()) {
+10            result.add(new ArrayList<>(path));
+11            return;
+12        }
+13        for (int end = start + 1; end <= s.length(); end++) {
+14            if (isPalindrome(s, start, end - 1)) {
+15                path.add(s.substring(start, end));
+16                backtrack(s, end, path, result);
+17                path.remove(path.size() - 1);
+18            }
+19        }
+20    }
+21
+22    private boolean isPalindrome(String s, int left, int right) {
+23        while (left < right) {
+24            if (s.charAt(left++) != s.charAt(right--)) {
+25                return false;
+26            }
+27        }
+28        return true;
+29    }
+30}
