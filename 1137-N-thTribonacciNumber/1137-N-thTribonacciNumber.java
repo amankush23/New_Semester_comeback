eea668a2c943988d782a66d6b42974b02b5d0df0
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-06 23:21:58 +0530

    File updated at 1/6/2026, 11:21:57 PM

--- 1137-N-thTribonacciNumber/1137-N-thTribonacciNumber.java
+++ 1137-N-thTribonacciNumber/1137-N-thTribonacciNumber.java
@@ -1,23 +1,23 @@
-// Last updated: 9/22/2025, 9:59:24 PM
-class Solution {
-    public int tribonacci(int n) {
-        int[] dp = new int[n+1];
-            Arrays.fill(dp, -1);
-            return tbn(n , dp);
-    }
-    public static int tbn(int n, int[] dp){
-        if(n == 0){
-            return 0;
-        }
-        if( n == 1 || n == 2){
-            return 1;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int t1 = tbn(n-1,dp);
-        int t2 = tbn(n-2,dp);
-        int t3 = tbn(n-3,dp);
-        return dp[n]=t1+t2+t3;
-    }
-}
+// Last updated: 1/6/2026, 11:21:57 PM
+1class Solution {
+2    public int tribonacci(int n) {
+3        int[] dp = new int[n+1];
+4            Arrays.fill(dp, -1);
+5            return tbn(n , dp);
+6    }
+7    public static int tbn(int n, int[] dp){
+8        if(n == 0){
+9            return 0;
+10        }
+11        if( n == 1 || n == 2){
+12            return 1;
+13        }
+14        if(dp[n]!=-1){
+15            return dp[n];
+16        }
+17        int t1 = tbn(n-1,dp);
+18        int t2 = tbn(n-2,dp);
+19        int t3 = tbn(n-3,dp);
+20        return dp[n]=t1+t2+t3;
+21    }
+22}
