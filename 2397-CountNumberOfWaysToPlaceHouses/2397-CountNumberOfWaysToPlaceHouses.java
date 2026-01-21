f8af07a65253784e8d13a0f7170e6725b389457a
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:57:13 +0530

    Create 2397-CountNumberOfWaysToPlaceHouses

---
+++ 2397-CountNumberOfWaysToPlaceHouses/2397-CountNumberOfWaysToPlaceHouses.java
@@ -0,0 +1,13 @@
+// Last updated: 21/01/2026, 11:57:13
+class Solution {
+    public int countHousePlacements(int n) {
+        int mod = 1000000007;
+        int[] dp = new int[n+1];
+        dp[0] =1;
+        dp[1] =2;
+        for(int i = 2; i <= n; i++){
+            dp[i] = (dp[i-1]+ dp[i-2])%mod;
+        }
+        return (int)((1L * dp[n]* dp[n])%mod);
+    }
+}
