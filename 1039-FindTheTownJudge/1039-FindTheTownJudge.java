9dc7bf0e8cd4d906cc9d2a74be495a31113f4502
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:59:20 +0530

    Create 1039-FindTheTownJudge

---
+++ 1039-FindTheTownJudge/1039-FindTheTownJudge.java
@@ -0,0 +1,18 @@
+// Last updated: 21/01/2026, 11:59:20
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int [] count = new int [n+1];
+        for(int i = 0 ; i < trust.length; i++){
+            int x = trust[i][0];;
+            int y = trust[i][1];;
+            count[x]--;
+            count[y]++;
+        }
+        for(int i = 1; i <= n ;i++){
+            if(count[i] == n-1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
