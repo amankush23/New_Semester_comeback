4da0412087d87004cf9949f21c8c47ba885056e2
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:56:01 +0530

    Create 3778-TransformArrayByParity

---
+++ 3778-TransformArrayByParity/3778-TransformArrayByParity.java
@@ -0,0 +1,34 @@
+// Last updated: 21/01/2026, 11:56:01
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public int[] transformArray(int[] nums) {
+        int counteven = 0;
+        int countodd = 0;
+        for(int i  = 0 ; i < nums.length; i++){
+            if(nums[i] % 2 != 0){
+                countodd++;
+            }
+            else{
+                counteven++;
+            }
+        }
+        List<Integer> list1 = new ArrayList<>();
+        for(int i = 0 ;  i < countodd; i++){
+            list1.add(1);
+        }
+        List<Integer> list2 = new ArrayList<>();
+        for(int i = 0 ;  i < counteven; i++){
+            list2.add(0);
+        }
+        List<Integer> combinedList = new ArrayList<>(list2);
+        combinedList.addAll(list1);
+
+        int[] result = new int[combinedList.size()];
+        for (int i = 0; i < combinedList.size(); i++) {
+            result[i] = combinedList.get(i);
+        }
+
+        return result;
+    }
+}
