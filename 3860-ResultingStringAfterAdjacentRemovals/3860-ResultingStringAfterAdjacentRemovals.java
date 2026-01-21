266cfb14a83727f0f150fde023bd1aa2a3413d4b
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:55:55 +0530

    Create 3860-ResultingStringAfterAdjacentRemovals

---
+++ 3860-ResultingStringAfterAdjacentRemovals/3860-ResultingStringAfterAdjacentRemovals.java
@@ -0,0 +1,18 @@
+// Last updated: 21/01/2026, 11:55:54
+class Solution {
+    public String resultingString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch:s.toCharArray()) {
+            if (sb.length()>0) {
+                char top = sb.charAt(sb.length()-1);
+                int diff = Math.abs(top-ch);
+                if (diff == 1 || diff == 25) {
+                    sb.deleteCharAt(sb.length()-1);
+                    continue;
+                }
+            }
+            sb.append(ch); 
+        }
+        return sb.toString();
+    }
+}
