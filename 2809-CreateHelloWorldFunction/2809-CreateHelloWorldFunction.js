1885b4db8f28c6e78a3e3b1fc46b327e34861ac9
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:56:43 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 21/01/2026, 11:56:43
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
