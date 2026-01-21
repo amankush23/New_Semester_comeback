eee398260df0ad3d77f3c4ea2546973074f7354c
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 12:00:12 +0530

    Create 653-TwoSumIvInputIsABst

---
+++ 653-TwoSumIvInputIsABst/653-TwoSumIvInputIsABst.java
@@ -0,0 +1,33 @@
+// Last updated: 21/01/2026, 12:00:11
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        Set<Integer> set = new HashSet<>();
+        return preorder(root, set , k);
+    }
+    public boolean preorder(TreeNode root, Set<Integer> set, int k ){
+        if(root == null){
+            return false;
+        }
+        int count = k - root.val;
+        if(set.contains(count)){
+            return true;
+        }
+        set.add(root.val);
+        return preorder(root.left, set, k) || preorder(root.right, set, k);
+    }
+}
