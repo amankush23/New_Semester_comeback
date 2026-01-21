61ddb9ac7f567d23ca9532afb2c986c87a67b4a5
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:59:32 +0530

    Create 967-MinimumFallingPathSum

---
+++ 967-MinimumFallingPathSum/967-MinimumFallingPathSum.java
@@ -0,0 +1,30 @@
+// Last updated: 21/01/2026, 11:59:32
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        
+        int ans = Integer.MAX_VALUE;
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        for(int[] a:dp){
+            Arrays.fill(a, -66666);
+        }
+        for(int col = 0 ; col < matrix[0].length; col++){
+            ans = Math.min(ans, Minimum_fal_sum(matrix, 0, col, dp));
+        }
+        return ans;
+    }
+    public static int Minimum_fal_sum(int [][] matrix, int cr, int cc, int[][] dp){
+        if(cc < 0 || cc >= matrix[0].length){
+            return Integer.MAX_VALUE;
+        }
+        if(cr == matrix.length-1){
+            return matrix[cr][cc];
+        }
+        if(dp[cr][cc] != -66666){
+            return dp[cr][cc];
+        }
+        int ld = Minimum_fal_sum(matrix, cr+1, cc-1,dp);
+        int rd = Minimum_fal_sum(matrix, cr+1, cc+1,dp);
+        int d = Minimum_fal_sum(matrix, cr+1, cc,dp);
+        return dp[cr][cc] = Math.min(rd, Math.min(ld, d))+matrix[cr][cc];
+    }
+}
