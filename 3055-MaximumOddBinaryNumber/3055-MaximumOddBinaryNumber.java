dce7be57a555bf8a407de4431ffdbc80718c4806
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:56:35 +0530

    Create 3055-MaximumOddBinaryNumber

---
+++ 3055-MaximumOddBinaryNumber/3055-MaximumOddBinaryNumber.java
@@ -0,0 +1,25 @@
+// Last updated: 21/01/2026, 11:56:34
+class Solution {
+    public String maximumOddBinaryNumber(String binaryString) {
+        int countOnes = 0;
+        StringBuilder resultBuilder = new StringBuilder();
+        
+        for (char ch : binaryString.toCharArray()) {
+            if (ch == '1') {
+                countOnes++;
+            }
+        }
+        
+        for (int i = 0; i < binaryString.length() - 1; i++) {
+            if (countOnes > 1) {
+                countOnes--;
+                resultBuilder.append('1');
+            } else {
+                resultBuilder.append('0');
+            }
+        }
+        
+        resultBuilder.append('1');
+        return resultBuilder.toString();
+    }
+}
