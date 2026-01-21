d6e5704a3b2ef353d5c88de62a5a92022173de7a
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:56:50 +0530

    Create 2670-MakeKSubarraySumsEqual

---
+++ 2670-MakeKSubarraySumsEqual/2670-MakeKSubarraySumsEqual.java
@@ -0,0 +1,17 @@
+// Last updated: 21/01/2026, 11:56:50
+class Solution {
+    public long makeSubKSumEqual(int[] arr, int k) {
+        long res = 0;
+        for (int i = 0; i < k; ++i) {
+            List<Integer> cycle = new ArrayList<>();
+            for (int j = i; arr[j] != 0; j = (j + k) % arr.length) {
+                cycle.add(arr[j]);
+                arr[j] = 0;
+            }
+            Collections.sort(cycle);
+            for (int n : cycle)
+                res += Math.abs(n - cycle.get(cycle.size() / 2));
+        }
+        return res;
+    }
+}
