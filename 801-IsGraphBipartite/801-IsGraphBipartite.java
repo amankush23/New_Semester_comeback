2fdee135c8c3f8d679cadd8b03ae4db694d13193
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:59:51 +0530

    Create 801-IsGraphBipartite

---
+++ 801-IsGraphBipartite/801-IsGraphBipartite.java
@@ -0,0 +1,53 @@
+// Last updated: 21/01/2026, 11:59:51
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        return IsBipartite(graph);
+    }
+    public static boolean IsBipartite(int[][] graph){
+        Queue<BipartitePair> q = new LinkedList<>();
+        HashMap<Integer, Integer> visited = new HashMap<>();
+        for (int i = 0; i < graph.length; i++) {
+            if(visited.containsKey(i)){
+                continue;
+
+            }
+            q.add(new BipartitePair(i, 0));
+            while(!q.isEmpty()){
+                //1.remove
+                BipartitePair rp = q.poll();
+                
+                //2. ignore if already visited
+                if(visited.containsKey(rp.vtx)){
+                    if(visited.get(rp.vtx)!=rp.dis){
+                        return false;
+                    }
+                    continue;
+                }
+                //3. marked visited
+                visited.put(rp.vtx, rp.dis); 
+                
+                //4. self work
+                
+                //5. add unvisited neighbours
+                for(int nbrs:graph[rp.vtx]){
+                    if(!visited.containsKey(nbrs)){
+                        q.add(new BipartitePair(nbrs, rp.dis+1));
+                    }
+
+                }
+                
+
+            }
+        }
+        return true;
+    }
+}
+class BipartitePair{
+    int vtx;
+    int dis;
+    public BipartitePair(int vtx, int dis){
+        this.vtx = vtx;
+        this.dis = dis;
+    }
+
+}
