153fdca4d0facd9ebc3436bf9680cf50c196d437
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:59:58 +0530

    Create 747-MinCostClimbingStairs

---
+++ 747-MinCostClimbingStairs/747-MinCostClimbingStairs.java
@@ -0,0 +1,21 @@
+// Last updated: 21/01/2026, 11:59:57
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp  = new int[cost.length];
+        Arrays.fill(dp, -1);
+        int zero = climbing_stairs(cost, 0,dp);
+        int one = climbing_stairs(cost, 1,dp);
+        return Math.min(zero, one);
+    }
+    public int climbing_stairs(int[] arr, int i, int[] dp){
+        if( i >= arr.length){
+            return 0;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        int f = climbing_stairs(arr, i+1,dp);
+        int s = climbing_stairs(arr, i+2,dp);
+        return dp[i]=Math.min(f,s)+arr[i];
+    }
+}
