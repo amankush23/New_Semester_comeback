881efda825079b0042328848235a98573b8f4006
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:59:25 +0530

    Create 1014-KClosestPointsToOrigin

---
+++ 1014-KClosestPointsToOrigin/1014-KClosestPointsToOrigin.java
@@ -0,0 +1,28 @@
+// Last updated: 21/01/2026, 11:59:25
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((e1, e2)->{
+            return Integer.compare(e2[0], e1[0]);
+        });
+        for(int i = 0; i < points.length; i++){
+            int[] point =points[i];
+            int x = point[0],y= point[1];
+            int dis = (x*x)+(y*y);
+            pq.add(new int[] {dis, i});
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+            int[][] res = new int[k][2];
+            int i = 0;
+            while( i < k){
+                int[] ele = pq.poll();
+                int idx = ele[1];
+                res[i][0] = points[idx][0];
+                res[i][1] = points[idx][1];
+                i++;
+            }
+            return res;
+    }
+    
+}
