68be1edc251fb6a36e2f5a3fc03bb419e200e016
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:57:19 +0530

    Create 2366-MaximumBagsWithFullCapacityOfRocks

---
+++ 2366-MaximumBagsWithFullCapacityOfRocks/2366-MaximumBagsWithFullCapacityOfRocks.java
@@ -0,0 +1,15 @@
+// Last updated: 21/01/2026, 11:57:18
+public class Solution {
+    public int maximumBags(int[] C, int[] R, int add) {
+        int n = R.length, cnt = 0;
+        for (int i = 0; i < n; i++) {
+            R[i] = C[i] - R[i];
+        }
+        Arrays.sort(R);
+        for (int i = 0; i < n && (R[i] - add) <= 0; i++) {
+            cnt++;
+            add -= R[i];
+        }
+        return cnt;
+    }
+}
