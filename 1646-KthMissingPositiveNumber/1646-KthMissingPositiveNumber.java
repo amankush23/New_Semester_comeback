88cf4b04b94f3380f4874ebc28501d005e79dec5
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:58:19 +0530

    Create 1646-KthMissingPositiveNumber

---
+++ 1646-KthMissingPositiveNumber/1646-KthMissingPositiveNumber.java
@@ -0,0 +1,33 @@
+// Last updated: 21/01/2026, 11:58:18
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+         
+        // Problem: Find the kth missing positive number from a sorted array
+        // Approach: Use Binary Search
+       // Idea: At index mid, number of missing elements = arr[mid] - (mid + 1)
+       // If missing < k  move right (low = mid + 1)
+       // Else  move left (high = mid - 1)
+      // Final answer = k + high + 1
+        
+        int n = arr.length;
+
+        int low = 0 , high = n -1;
+
+        while(low <= high){
+
+            int mid = (low + high )/2;
+
+            int missing = arr[mid] - (mid + 1);
+
+            if(missing < k ){
+                low = mid + 1;
+
+            }else{
+                high = mid - 1;
+            }
+        }
+
+        return k + high + 1;
+        
+    }
+}
