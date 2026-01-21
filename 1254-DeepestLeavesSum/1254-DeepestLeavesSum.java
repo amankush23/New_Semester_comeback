c0c108f8714cee7c8794cd7f5f413784b855ca44
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:59:01 +0530

    Create 1254-DeepestLeavesSum

---
+++ 1254-DeepestLeavesSum/1254-DeepestLeavesSum.java
@@ -0,0 +1,24 @@
+// Last updated: 21/01/2026, 11:59:00
+class Solution {
+    public int deepestLeavesSum(TreeNode root) {
+        if (root == null) return 0;
+
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        int sum = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            sum = 0; // reset sum for each level
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                sum += node.val;
+
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+        }
+        return sum; // last level sum
+    }
+}
