976c9d45fed6d1f8b377cf219d947bc1ced9a065
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:58:11 +0530

    Create 1708-DesignParkingSystem

---
+++ 1708-DesignParkingSystem/1708-DesignParkingSystem.java
@@ -0,0 +1,16 @@
+// Last updated: 21/01/2026, 11:58:11
+class ParkingSystem {
+    private int arr[];
+    public ParkingSystem(int big, int medium, int small) {
+        arr = new int[]{big,medium,small};
+    }
+    
+    public boolean addCar(int carType) {
+        if(arr[carType-1]>0)
+        {   
+            arr[carType-1]--;
+            return true;
+        }
+        return false;
+    }
+}
