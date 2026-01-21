b78c552ae56fa093712ee4db8d5cd3ab090706cc
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:59:45 +0530

    Create 902-MinimumNumberOfRefuelingStops

---
+++ 902-MinimumNumberOfRefuelingStops/902-MinimumNumberOfRefuelingStops.java
@@ -0,0 +1,17 @@
+// Last updated: 21/01/2026, 11:59:45
+class Solution {
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int fuel = startFuel, stops = 0, i = 0, n = stations.length;
+        while (fuel < target) {
+            while (i < n && stations[i][0] <= fuel) {
+                pq.add(stations[i][1]);
+                i++;
+            }
+            if (pq.isEmpty()) return -1;
+            fuel += pq.poll();
+            stops++;
+        }
+        return stops;
+    }
+}
