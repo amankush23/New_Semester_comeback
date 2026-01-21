2f39b876b8962e6affea39d211583829d5fb777e
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:58:58 +0530

    Create 1320-RemoveAllAdjacentDuplicatesInStringIi

---
+++ 1320-RemoveAllAdjacentDuplicatesInStringIi/1320-RemoveAllAdjacentDuplicatesInStringIi.java
@@ -0,0 +1,37 @@
+// Last updated: 21/01/2026, 11:58:58
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        return removeDuplicate(s, k);
+    }
+    public String removeDuplicate(String s, int k) {
+        Stack<Character> charStack = new Stack<>();
+        Stack<Integer> countStack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (!charStack.isEmpty() && charStack.peek() == ch) {
+                countStack.push(countStack.pop() + 1);
+            } else {
+                charStack.push(ch);
+                countStack.push(1);
+            }
+
+            if (countStack.peek() == k) {
+                charStack.pop();
+                countStack.pop();
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!charStack.isEmpty()) {
+            int count = countStack.pop();
+            char ch = charStack.pop();
+            for (int i = 0; i < count; i++) {
+                sb.insert(0, ch);
+            }
+        }
+
+        return sb.toString();
+    }
+}
