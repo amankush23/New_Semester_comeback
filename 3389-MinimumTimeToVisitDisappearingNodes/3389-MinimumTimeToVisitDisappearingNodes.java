e01642632bc5d723e2f635d435a0b68806b9d1f8
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-01-21 11:56:22 +0530

    Create 3389-MinimumTimeToVisitDisappearingNodes

---
+++ 3389-MinimumTimeToVisitDisappearingNodes/3389-MinimumTimeToVisitDisappearingNodes.java
@@ -0,0 +1,63 @@
+// Last updated: 21/01/2026, 11:56:22
+class Solution {
+    public int[] minimumTime(int n, int[][] edges, int[] disappear) {
+        HashMap<Integer, HashMap<Integer,Integer>>map = new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            map.put(i,new HashMap<>());
+        }
+
+        for(int e[] : edges){
+            int e1 = e[0];
+            int e2 = e[1];
+            int cost = e[2];
+            map.get(e1).put(e2,Math.min(cost,map.get(e1).getOrDefault(e2,Integer.MAX_VALUE)));
+            map.get(e2).put(e1,Math.min(cost,map.get(e2).getOrDefault(e1,Integer.MAX_VALUE)));
+        }
+
+        HashSet<Integer>visited = new HashSet<>();
+        PriorityQueue<Pair>pq = new PriorityQueue<>((a,b)->{
+            return a.cost-b.cost;
+        });
+        pq.add(new Pair(0,0));
+        int dist[] = new int[n];
+        Arrays.fill(dist,-1);
+        dist[0]=0;
+
+        while(!pq.isEmpty()){
+            // remove
+            Pair rm = pq.poll();
+
+            // check
+            if(visited.contains(rm.vtx)){
+                continue;
+            }
+            
+            // mark visited
+            visited.add(rm.vtx);
+
+            // self work
+            if(rm.cost>=disappear[rm.vtx]){
+                continue;  
+            }
+            dist[rm.vtx] = rm.cost;
+
+            // add ngbrs
+            for(int ngbr : map.get(rm.vtx).keySet()){
+                if(!visited.contains(ngbr)){
+                    pq.add(new Pair(ngbr, rm.cost+map.get(rm.vtx).get(ngbr)));
+                }
+            }
+        }
+        return dist;
+    }
+
+    class Pair{
+        int vtx;
+        int cost;
+        public Pair(int vtx, int cost){
+            this.vtx = vtx;
+            this.cost = cost;
+        }
+    }
+}
