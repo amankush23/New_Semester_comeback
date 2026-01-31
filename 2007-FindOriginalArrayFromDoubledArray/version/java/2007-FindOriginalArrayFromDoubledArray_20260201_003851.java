79e0ebb15e387f0e6637784137ed771ff774712a
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-02-01 00:38:51 +0530

    Create 2007-FindOriginalArrayFromDoubledArray

---
+++ 2007-FindOriginalArrayFromDoubledArray/version/java/2007-FindOriginalArrayFromDoubledArray_20260201_003851.java
@@ -0,0 +1,29 @@
+// Last updated: 01/02/2026, 00:38:51
+1class Solution {
+2    public int[] findOriginalArray(int[] changed) {
+3        if(changed.length == 1){
+4            return new int[]{};
+5        }
+6        Arrays.sort(changed);
+7        List<Integer> ans = new ArrayList<>();
+8        Queue<Integer> q = new ArrayDeque<>();
+9
+10        for(int i = 0; i < changed.length; i++){
+11            if(q.size() > 0 && q.peek() == changed[i]){
+12                q.remove();
+13            }else{
+14                ans.add(changed[i]);
+15                q.offer(changed[i] * 2);
+16            }
+17        }
+18        if(q.size() > 0){
+19            return new int[]{};
+20        }
+21
+22        int arr[] = new int[ans.size()];
+23        for(int i = 0; i < ans.size(); i++){
+24            arr[i] = ans.get(i);
+25        }
+26        return arr;
+27    }
+28}
