447e40e0d6b59970012c769b1fe77c78efe5eb1c
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-02-03 11:29:18 +0530

    Create 1019-NextGreaterNodeInLinkedList

---
+++ 1019-NextGreaterNodeInLinkedList/version/java/1019-NextGreaterNodeInLinkedList_20260203_112918.java
@@ -0,0 +1,31 @@
+// Last updated: 03/02/2026, 11:29:18
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public int[] nextLargerNodes(ListNode head) {
+13        ArrayList<Integer> ll = new ArrayList<>();
+14        ListNode temp = head;
+15        while(temp!=null){
+16            ll.add(temp.val);
+17            temp = temp.next;
+18        }
+19        Stack<Integer> st = new Stack<>();
+20        int n = ll.size();
+21        int ans[] = new int[n];
+22        for(int i=0;i<n;i++){
+23            while(!st.isEmpty() && ll.get(i)>ll.get(st.peek())){
+24                ans[st.pop()]=ll.get(i);
+25            }
+26            st.push(i);
+27        }
+28        return ans;
+29    }
+30}
