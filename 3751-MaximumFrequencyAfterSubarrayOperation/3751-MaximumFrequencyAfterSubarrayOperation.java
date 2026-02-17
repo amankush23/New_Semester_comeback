8e42dc64367f784913c391d7ec3eec7584eb2741
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-02-17 10:46:06 +0530

    Create 3751-MaximumFrequencyAfterSubarrayOperation

---
+++ 3751-MaximumFrequencyAfterSubarrayOperation/3751-MaximumFrequencyAfterSubarrayOperation.java
@@ -0,0 +1,11 @@
+// Last updated: 17/02/2026, 10:46:06
+public class Solution{
+public int maxFrequency(int[] A , int k) {
+        int count[] = new int[51], res = 0;
+        for (int a : A) {
+            count[a] = Math.max(count[a], count[k]) + 1;
+            res = Math.max(res, count[a] - count[k]);
+        }
+        return count[k] + res;
+    }
+}
