16cf12cb85887ba91ed2a26f1651b6c48fedfa40
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-04-12 15:05:49 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/version/java/70-ClimbingStairs_20260412_150548.java
@@ -0,0 +1,14 @@
+// Last updated: 12/04/2026, 15:05:48
+1class Solution {
+2    public int climbStairs(int n) {
+3        HashMap<Integer,Integer> map = new HashMap<>();
+4        return solve(n, map);
+5    }
+6    public int solve(int n, HashMap<Integer,Integer> map){
+7        if(n == 0 || n == 1) return 1;
+8        if(!map.containsKey(n)){
+9            map.put(n, solve(n-1, map) + solve(n-2, map));
+10        }
+11        return map.get(n);
+12    }
+13}
