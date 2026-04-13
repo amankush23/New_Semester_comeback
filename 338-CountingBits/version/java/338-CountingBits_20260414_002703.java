0390ff1652c014811b1c236194a4d4a9e93a4ed9
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-04-14 00:27:04 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/version/java/338-CountingBits_20260414_002703.java
@@ -0,0 +1,22 @@
+// Last updated: 14/04/2026, 00:27:03
+1class Solution {
+2    public int[] countBits(int n) {
+3        return solve(n);
+4    }
+5    public int[] solve(int n){
+6        int[] arr = new int[n+1];
+7
+8        for(int i = 0; i <= n;i++){
+9            int count = 0;
+10            int num = i;
+11            while(num > 0){
+12                if((num & 1) != 0){
+13                    count++;
+14                }
+15                num = num >> 1;
+16            }
+17            arr[i] = count;
+18        }
+19        return arr;
+20    }
+21}
