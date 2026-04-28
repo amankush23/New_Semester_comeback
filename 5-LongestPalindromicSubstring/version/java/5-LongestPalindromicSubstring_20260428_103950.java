ddff6ee95d1d0e95bde257434cbde97d1682f9e3
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-04-28 10:39:51 +0530

    Create 5-LongestPalindromicSubstring

---
+++ 5-LongestPalindromicSubstring/version/java/5-LongestPalindromicSubstring_20260428_103950.java
@@ -0,0 +1,32 @@
+// Last updated: 28/04/2026, 10:39:50
+1class Solution {
+2    public String longestPalindrome(String s) {
+3        return longestPalindromicStr(s);
+4    }
+5
+6    public static String longestPalindromicStr(String str) {
+7        String longest = "";
+8        for (int i = 0; i < str.length(); i++) {
+9            for (int j = i; j < str.length(); j++) { // Fixed loop range
+10                String sub = str.substring(i, j + 1); // j+1 to include last character
+11                if (isPalindrom(sub) && sub.length() > longest.length()) {
+12                    longest = sub;
+13                }
+14            }
+15        }
+16        return longest;
+17    }
+18
+19    public static boolean isPalindrom(String s) {
+20        int i = 0, j = s.length() - 1; // Fix j index
+21        while (i < j) {
+22            if (s.charAt(i) != s.charAt(j)) {
+23                return false;
+24            }
+25            i++;
+26            j--;
+27        }
+28        return true;
+29    }
+30}
+31
