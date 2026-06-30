60f06c97f6dbeddd9e6a59c2395caf31d78a2f9f
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2026-06-30 21:52:24 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/version/java/242-ValidAnagram_20260630_215223.java
@@ -0,0 +1,10 @@
+// Last updated: 30/06/2026, 21:52:23
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3        char[] arr1 = s.toCharArray();
+4        char[] arr2 = t.toCharArray();
+5        Arrays.sort(arr1);
+6        Arrays.sort(arr2);
+7        return Arrays.equals(arr1, arr2);
+8    }
+9}
